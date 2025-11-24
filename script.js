32ff6715665f7be07462d28219bbe157896ea555
Heloísa <heloisadepaula813@gmail.com>
2025-11-23 22:58:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
     // --- CONFIGURAÇÃO ---
-    const GEMINI_API_KEY = "AIzaSyBpB8rfCFmlfxtCY48YrqiC1mVueE80P3Y"; 
+    const GEMINI_API_KEY = "GEMINI_API_KEY"; 
     const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
@@ -453,6 +453,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
     // Load Profile from Storage
 
     preencherFormulario();
+
 
 
 
